a16805cebd6e75a1c6b10c102f125b5ace869fd3
Katerina <katerina4717@gmail.com>
2019-09-05 20:19:45 +0300

    change className

--- src/App.js
+++ src/App.js
@@ -3,8 +3,48 @@ import React from 'react'
 const App = () => {
 	return (
 		<div>
-			<h1>Hello world</h1>
-			<p>hjkjgfjbjbfj</p>
+			<header class="header">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="logo">
+								<img src="img/logo.png" alt=""/>
+						</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="menu">
+									<ul>
+										<li><a href="">Home</a></li>
+										<li><a href="">Products</a></li>
+										<li><a href="">Payment</a></li>
+										<li><a href="">Shipping</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="cart text-center">
+									<div class="products-count">0</div>
+									<div class="products-price">$500</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			</header>
+				<main class="main">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-3">
+
+							</div>
+							<div class="col-lg-9">
+
+							</div>
+						</div>
+					</div>
+				</main>
+				<footer class="footer">
+
+				</footer>
 		</div>
 	)
 }

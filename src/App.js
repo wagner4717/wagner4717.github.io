d461f78221c01798748ae247d58a606b996e0919
Katerina <katerina4717@gmail.com>
2019-09-05 20:49:40 +0300

    creaded Header, Main, Footer

--- src/App.js
+++ src/App.js
@@ -6,51 +6,72 @@ import './header.css'
 import './menu.css'
 import './cart.css'
 
-const App = () => {
+const Header = () => {
 	return (
-		<div>
 			<header className="header">
 				<div className="container">
 					<div className="row">
 						<div className="col-lg-3">
 							<div className="logo">
-								<img src="img/logo.png" alt=""/>
+								<img src="img/logo.png" alt="" />
+							</div>
 						</div>
+						<div className="col-lg-6">
+							<div className="menu">
+								<ul>
+									<li><a href="/">Home</a></li>
+									<li><a href="/">Products</a></li>
+									<li><a href="/">Payment</a></li>
+									<li><a href="/">Shipping</a></li>
+								</ul>
 							</div>
-							<div className="col-lg-6">
-								<div className="menu">
-									<ul>
-										<li><a href="/">Home</a></li>
-										<li><a href="/">Products</a></li>
-										<li><a href="/">Payment</a></li>
-										<li><a href="/">Shipping</a></li>
-									</ul>
-								</div>
-							</div>
-							<div className="col-lg-3">
-								<div className="cart text-center">
-									<div className="products-count">0</div>
-									<div className="products-price">$500</div>
-								</div>
+						</div>
+						<div className="col-lg-3">
+							<div className="cart text-center">
+								<div className="products-count">0</div>
+								<div className="products-price">$500</div>
 							</div>
 						</div>
 					</div>
+				</div>
 			</header>
-				<main className="main">
-					<div className="container">
-						<div className="row">
-							<div className="col-lg-3">
+	)
+}
 
-							</div>
-							<div className="col-lg-9">
+const Main = () => {
+	return (
+			<main className="main">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
 
-							</div>
+						</div>
+						<div className="col-lg-9">
+
 						</div>
 					</div>
-				</main>
-				<footer className="footer">
+				</div>
+			</main>
+	)
+}
 
-				</footer>
+
+const Footer = () => {
+	return (
+			<footer className="footer">
+              Footer
+			</footer>
+
+	)
+}
+
+
+const App = () => {
+	return (
+		<div>
+			<Header/>
+			<Main/>
+			<Footer/>
 		</div>
 	)
 }

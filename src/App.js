1971f1b71bf88e14defe5b5efab364dfaccc3e58
Katerina <katerina4717@gmail.com>
2019-09-05 21:07:10 +0300

    added Header, Main, Footer

--- src/App.js
+++ src/App.js
@@ -5,66 +5,9 @@ import './base.css'
 import './header.css'
 import './menu.css'
 import './cart.css'
-
-const Header = () => {
-	return (
-			<header className="header">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-3">
-							<div className="logo">
-								<img src="img/logo.png" alt="" />
-							</div>
-						</div>
-						<div className="col-lg-6">
-							<div className="menu">
-								<ul>
-									<li><a href="/">Home</a></li>
-									<li><a href="/">Products</a></li>
-									<li><a href="/">Payment</a></li>
-									<li><a href="/">Shipping</a></li>
-								</ul>
-							</div>
-						</div>
-						<div className="col-lg-3">
-							<div className="cart text-center">
-								<div className="products-count">0</div>
-								<div className="products-price">$500</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</header>
-	)
-}
-
-const Main = () => {
-	return (
-			<main className="main">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-3">
-
-						</div>
-						<div className="col-lg-9">
-
-						</div>
-					</div>
-				</div>
-			</main>
-	)
-}
-
-
-const Footer = () => {
-	return (
-			<footer className="footer">
-              Footer
-			</footer>
-
-	)
-}
-
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
 
 const App = () => {
 	return (
@@ -75,5 +18,6 @@ const App = () => {
 		</div>
 	)
 }
+
 
 export default App
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main className="main">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3">
+
+                    </div>
+                    <div className="col-lg-9">
+
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main

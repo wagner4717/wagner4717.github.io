c483a3637f73d338044bd045799f893e9db59675
Katerina <katerina4717@gmail.com>
2019-09-12 19:57:08 +0300

    added destr to ProductListItem

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 import "./ProductListItem.css"
 
-const ProductListItem =(props) => {
+const ProductListItem =({
+    name,
+    description,
+    type,
+    capacity,
+    price,
+}) => {
     return (
         <div className="product-list-item"> 
-           <div className="product-title">{props.name}</div>
-           <p className="product-description">{props.description}</p>
-            <div className="product-features">Type: {props.type}</div>
-            <div className="product-features">Capacity: {props.capacity}</div>
-            <div className="product-price">Price: {props.price}</div>
+           <div className="product-title">{name}</div>
+           <p className="product-description">{description}</p>
+            <div className="product-features">Type: {type}</div>
+            <div className="product-features">Capacity: {capacity}</div>
+            <div className="product-price">Price: {price}</div>
             <button className="btn btn-add-to-cart"> Add to cart</button>
         </div>
     )

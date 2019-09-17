43a68c9086afa909f649a5a746a667e7d12615bd
Katerina <katerina4717@gmail.com>
2019-09-17 19:49:53 +0300

    change to class Component

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,33 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types';
 import "./ProductListItem.css"
 
-const ProductListItem =({
-    name,
-    description = "No description",
-    type,
-    capacity,
-    price,
-    image,
-}) => {
-    return (
-        <div className="product-list-item"> 
-            <div className="product-image">
-                <img src={image} alt={description}/>
+
+class ProductListItem extends Component {
+    render() {
+        return (
+            <div className="product-list-item">
+                <div className="product-image">
+                    <img src={this.image} alt={this.description} />
                 </div>
-           <div className="product-title">{name}</div>
-           <p className="product-description">{description}</p>
-            <div className="product-features">Type: {type}</div>
-            <div className="product-features">Capacity: {capacity}G</div>
-            <div className="product-price">Price: {price}$</div>
-            <div className="quantity-input">
-                <button>-</button>
-                <input type="text"/>
-                <button>+</button>
+                <div className="product-title">{this.name}</div>
+                <p className="product-description">{this.description}</p>
+                <div className="product-features">Type: {this.type}</div>
+                <div className="product-features">Capacity: {this.capacity}G</div>
+                <div className="product-price">Price: {this.price}$</div>
+                <div className="quantity-input">
+                    <button>-</button>
+                    <input type="text" value="1" />
+                    <button>+</button>
+                </div>
+                <button className="btn btn-add-to-cart"> Add to cart</button>
             </div>
-            <button className="btn btn-add-to-cart"> Add to cart</button>
-        </div>
-    )
+        )
+    }
 }
 
 

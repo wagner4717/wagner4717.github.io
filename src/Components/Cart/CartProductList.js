66d0433812b3eacb83e25feac303c13c0f3f7477
Katerina <katerina4717@gmail.com>
2019-10-17 20:24:04 +0300

    added to CartProductList component CartProductListItem, and made component CartListItemComponent

--- src/Components/Cart/CartProductList.js
+++ src/Components/Cart/CartProductList.js
@@ -1,19 +1,23 @@
 import React from 'react'
-
 import { keys } from 'lodash'
 import productsData, { getProductsMap } from '../../App/Main/Products/productsData'
+import CartProductListItem from './CartProductListItem';
 
 
 const CartProductList = ({
     productsInCart,
     productsMap = getProductsMap(productsData),
+    CartListItemComponent=CartProductListItem
 }) => {
     return (
         <div>
             {
                 keys(productsInCart).map((productId) => {
                     return (
-                        <div key={productId}>{productsMap[productId].name}: {productsInCart[productId]}</div>
+                        <CartListItemComponent
+                            product={productsMap[productId]}
+                            productCount={productsInCart[productId]}
+                        />
                     )
                 })
             }
